053ec4c3d0770c9b9b28fb7374b7df2fee1e39fd
Mon <Simon-freund@outlook.de>
2023-11-14 17:16:18 +0100

    1st day done

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+
+    }
+}
