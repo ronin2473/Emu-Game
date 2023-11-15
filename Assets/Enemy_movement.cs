e39aa466415fad451a14289dfb9f1fee5ffba2c4
MrPowerpoint <tiziand4404@gmail.com>
2023-11-15 13:22:08 +0100

    complex enemy movement

--- Assets/Enemy_movement.cs
+++ Assets/Enemy_movement.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using static Unity.VisualScripting.Member;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class Enemy_movement : MonoBehaviour
 {
@@ -18,19 +19,41 @@ public class Enemy_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rigidbody rigi = this.GetComponent<Rigidbody>();
-        rigi.velocity = Vector3.zero;
-        Vector3 direction = Getposition();
-        direction.x = Math.Sign(direction.x - this.transform.position.x);
-        direction.y = Math.Sign(direction.y - this.transform.position.y);
-        direction.z = Math.Sign(direction.z - this.transform.position.z);
-        rigi.AddForce(direction*speed);
+
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            MoveEnemy(enemy);
+        }
+        
+        
+        
+        //Rigidbody rigi = this.GetComponent<Rigidbody>();
+        //rigi.velocity = Vector3.zero;
+        //Vector3 direction = Getposition();
+        //direction.x = Math.Sign(direction.x - this.transform.position.x);
+        //direction.y = Math.Sign(direction.y - this.transform.position.y);
+        //direction.z = Math.Sign(direction.z - this.transform.position.z);
+        //rigi.AddForce(direction*speed);
         
     }
 
     public Vector3 Getposition() {
         GameObject goal = GameObject.FindGameObjectWithTag("Player");
         return goal.transform.position;
-        
+    }
+
+    public void MoveEnemy(GameObject enemy)
+    {
+        Rigidbody rigi = enemy.GetComponent<Rigidbody>();
+        rigi.velocity = Vector3.zero;
+        Vector3 direction = Getposition();
+        direction.x = Math.Sign(direction.x - enemy.transform.position.x);
+        direction.y = Math.Sign(direction.y - enemy.transform.position.y);
+        direction.z = Math.Sign(direction.z - enemy.transform.position.z);
+        rigi.AddForce(direction * speed);
+
     }
 }
+
