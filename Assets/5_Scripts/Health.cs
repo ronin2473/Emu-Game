ef05bc4572ebac6be1c41bb5dc9ee9521212ee4d
Mon <Simon-freund@outlook.de>
2023-11-29 16:35:15 +0100

    Health Bar

--- Assets/5_Scripts/Health.cs
+++ Assets/5_Scripts/Health.cs
@@ -6,10 +6,13 @@ public class Health : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
+
+    public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(int damageAmount)
@@ -19,5 +22,6 @@ public class Health : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        healthBar.SetHealth(currentHealth);
     }
 }
