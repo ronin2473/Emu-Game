dded797df0056bdaf429b19a3a1666e38f3ffd97
Mon <Simon-freund@outlook.de>
2023-11-30 17:30:42 +0100

    Enemy Health

---
+++ Assets/5_Scripts/EnemyHealth.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
