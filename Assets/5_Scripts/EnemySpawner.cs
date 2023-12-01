27a91d238c1af977956ffe06c16aa9575e7e3874
Mon <Simon-freund@outlook.de>
2023-12-01 17:25:57 +0100

    Enemy Spawn without Pooling

---
+++ Assets/5_Scripts/EnemySpawner.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public string waveName;
+        public List<EnemyGroup> enemyGroups;
+        public int waveQuota;
+        public float spawnInterval;
+        public int spawncount;
+    }
+
+    [System.Serializable]
+    public class EnemyGroup
+    {
+        public string enemyName;
+        public int enemyCount;
+        public int spawnCount;
+        public GameObject enemyPrefab;
+    }
+
+    public List<Wave> waves;
+    public int currentWaveCount;
+
+    [Header("Spawner Attributes")]
+    float spawnTimer;
+    public int enemiesAlive;
+    public int maxEnemiesAllowed;
+    public bool maxEnemiesReached = false;
+    public float waveInterval;
+
+    [Header("Spawn positions")]
+    public List<Transform> relativeSpawnPoints;
+
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player_Movement>().transform;
+        CalculateWaveQuota();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentWaveCount < waves.Count && waves [currentWaveCount].spawncount == 0)
+        {
+            StartCoroutine(BeginNextWave());
+        }
+
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= waves[currentWaveCount].spawnInterval)
+        {
+            spawnTimer = 0f;
+            SpawnEnemies();
+
+        }
+    }
+    
+    IEnumerator BeginNextWave()
+    {
+        yield return new WaitForSeconds(waveInterval);
+
+        if (currentWaveCount < waves.Count - 1)
+        {
+            currentWaveCount++;
+            CalculateWaveQuota();
+        }
+    }
+
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach (var EnemyGroup in waves[currentWaveCount].enemyGroups)
+        {
+            currentWaveCount += EnemyGroup.enemyCount;
+        }
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+        Debug.LogWarning(currentWaveQuota);
+    }
+
+    void SpawnEnemies()
+    {
+        if (waves[currentWaveCount].spawncount < waves[currentWaveCount].waveQuota && !maxEnemiesReached)
+        {
+            foreach (var enemyGroup in waves[currentWaveCount].enemyGroups)
+            {
+                if (enemyGroup.spawnCount < enemyGroup.enemyCount)
+                {
+                    if (enemiesAlive >= maxEnemiesAllowed)
+                    {
+                        maxEnemiesReached = true;
+                        return;
+                    }
+
+                    Instantiate(enemyGroup.enemyPrefab, player.position + relativeSpawnPoints[Random.Range(0, relativeSpawnPoints.Count)].position, Quaternion.identity);
+
+                    enemyGroup.spawnCount++;
+                    waves[currentWaveCount].spawncount++;
+                    enemiesAlive++;
+                }
+            }
+        }
+
+        if (enemiesAlive < maxEnemiesAllowed)
+        {
+            maxEnemiesReached = false;
+        }
+    }
+
+    public void OnEnemyKilled()
+    {
+        enemiesAlive--;
+    }
+}
--- Assets/5_Scripts/EnemyStats.cs
+++ Assets/5_Scripts/EnemyStats.cs
@@ -6,6 +6,22 @@ public class EnemyStats : MonoBehaviour
 {
     [SerializeField]  float currentHealth = 50;
     float waitedtime = 0.5f;
+
+    public float despawnDistance = 20f;
+    Transform player;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player_Movement>().transform;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) >= despawnDistance) 
+        {
+            ReturnEnemy();
+        }
+    }
     public void takedamage(float amount)
     {
         waitedtime += Time.deltaTime;
@@ -13,10 +29,24 @@ public class EnemyStats : MonoBehaviour
             waitedtime= 0;
             currentHealth -= amount;
             if (currentHealth <= 0) {
-                
-                this.gameObject.SetActive(false);
+
+                //this.gameObject.SetActive(false);
+                Destroy(gameObject);
             
             }
         }
     }
+
+    private void OnDestroy()
+    {
+        EnemySpawner es = FindObjectOfType<EnemySpawner>();
+        es.OnEnemyKilled();
+    }
+
+    void ReturnEnemy()
+    {
+        EnemySpawner es = FindObjectOfType<EnemySpawner>();
+        transform.position = player.position + es.relativeSpawnPoints[Random.Range(0, es.relativeSpawnPoints.Count)].position;
+    }
 }
+
