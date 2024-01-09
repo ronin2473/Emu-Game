aa252e7b7617756a492d133676439637bcebc807
MrPowerpoint <tiziand4404@gmail.com>
2024-01-09 15:05:48 +0100

    we got weapons, pogies

--- Assets/5_Scripts/OnHitDo.cs
+++ Assets/5_Scripts/OnHitDo.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class OnHitDo : MonoBehaviour
 {
-    [SerializeField] TestWeaponBehaviour weaponManager;
+    [SerializeField] public float swingtime;
     private void OnTriggerStay2D(Collider2D enemy)
     {
-
 
         if (enemy.gameObject.tag == "Enemy")
         {
             EnemyStats stats = enemy.GetComponent<EnemyStats>();
-            stats.takedamage(25,weaponManager.weaponSwingTime);
+            stats.takedamage(25, swingtime);
         }
     }
 }
