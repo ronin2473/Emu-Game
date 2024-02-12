f40f3461335048dd6160680f55fb2674b4ffca0b
MrPowerpoint <tiziand4404@gmail.com>
2024-02-12 16:48:47 +0100

    Can now move between buttons using the magic of a keyboard or controller

--- Assets/5_Scripts/ButtonTraversing.cs
+++ Assets/5_Scripts/ButtonTraversing.cs
@@ -26,91 +26,117 @@ public class ButtonTraversing : MonoBehaviour
     public int ymax;
     public int x;
     public  int y;
+    bool keyboard = false;
 
     // Update is called once per frame
     void Update()
     {
         string[] joystickNames = Input.GetJoystickNames();
-        Debug.Log(joystickNames.Length);
+        if (Input.anyKey && !Input.GetButtonDown("Fire1") && !Input.GetButtonDown("Fire3") && !Input.GetButtonDown("Fire2"))
+        {
+            keyboard = true;
+        }
         
-        if (Input.GetAxis("Vertical") != 0 && joystickNames.Length > 0)
+            if (Mathf.Abs(Input.GetAxis("Mouse X")) > 0 && Mathf.Abs(Input.GetAxis("Mouse Y")) > 0)
         {
-            y += ((int)Mathf.Sign(Input.GetAxis("Vertical")));
-            if (y > ymax)
+            if (keyboard)
             {
-                y = 0;
-            }
-            else if (y < 0)
-            {
-                y = ymax;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Debug.Log("test");
-            y += 1;
-            if (y > ymax)
-            {
-                y = 0;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            y -= 1;
-            if (y < 0)
-            {
-                y = ymax;
-            }
-        }
+                PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+                foreach (Buttonv2 v in buttons)
+                {
 
-        if (Input.GetAxis("Horizontal") != 0 && joystickNames.Length > 0)
-        {
-            x += ((int)Mathf.Sign(Input.GetAxis("Horizontal")));
+                    v.button.OnPointerExit(pointerEventData);
+                }
+                
+            }
+            x = 1;
+            y = 1;
+
+            keyboard = false;
+            
+        }
+        if (keyboard)
+        { 
+            if (Input.GetAxis("Vertical") != 0 && joystickNames.Length > 0)
+            {
+                y += ((int)Mathf.Sign(Input.GetAxis("Vertical")));
+                if (y > ymax)
+                {
+                    y = 0;
+                }
+                else if (y < 0)
+                {
+                    y = ymax;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                y += 1;
+                
+            }
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                y -= 1;
+                
+            }
+
+            if (Input.GetAxis("Horizontal") != 0 && joystickNames.Length > 0)
+            {
+                x += ((int)Mathf.Sign(Input.GetAxis("Horizontal")));
+                if (x > xmax)
+                {
+                    x = 0;
+                }
+                 else if (x < 0)
+                {
+                    x = xmax;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                x += 1;
+                
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                x -= 1;
+                
+            }
+            foreach (Buttonv2 button in buttons)
+            {
+                if (button.x == x &&  button.y == y) {
+                    PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+                    foreach (Buttonv2 v in buttons)
+                    {
+                    
+                        v.button.OnPointerExit(pointerEventData);
+                            }
+                    selectedbutton = button.button;
+                
+                    button.button.OnPointerEnter(pointerEventData);
+                    break;
+                }
+            }
             if (x > xmax)
             {
                 x = 0;
             }
-             else if (x < 0)
-            {
-                x = xmax;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            x += 1;
-            if (x > xmax)
-            {
-                x = 0;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            x -= 1;
             if (x < 0)
             {
                 x = xmax;
             }
-        }
-        foreach (Buttonv2 button in buttons)
-        {
-            if (button.x == x &&  button.y == y) {
-                PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
-                foreach (Buttonv2 v in buttons)
-                {
-                    
-                    v.button.OnPointerExit(pointerEventData);
-                        }
-                selectedbutton = button.button;
-                
-                button.button.OnPointerEnter(pointerEventData);
-                break;
+            if (y < 0)
+            {
+                y = ymax;
+            }
+            if (y > ymax)
+            {
+                y = 0;
+            }
+            if (Input.GetButtonDown("Submit"))
+            {
+                selectedbutton.onClick.Invoke();
             }
         }
-        
-        if (Input.GetButtonDown("Submit"))
-        {
-            selectedbutton.onClick.Invoke();
-        }
-
     }
 }
